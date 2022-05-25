70eba63c6fdbb709444b675e5531f5c04a479a9c
truongducvien <truongducvien97@gmail.com>
2022-05-25 19:30:02 +0700

    first commit

---
+++ src/render2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import Style from './2305style/2305';
+
+// let root2 = ReactDOM.createRoot(document.getElementById('root2'))
+
+export default function Demo(props){ 
+      // console.log(props)
+   return(
+      <h3>this is render2.js</h3>
+   )
+}
