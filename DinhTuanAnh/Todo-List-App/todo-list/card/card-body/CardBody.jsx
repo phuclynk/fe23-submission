09745cc84b29360d34b2002fe928ce052922d55f
tuananh7 <dinhtuananh.es@gmail.com>
2022-05-25 19:51:40 +0700

    todo-list-app

--- DinhTuanAnh/Todo-List-App/todo-list/card/card-body/CardBody.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./style.css";
-import { TaskList } from "../../task-list/TaskList";
-
-export function CardBody() {
-    return <div className="card-body">
-        <TaskList></TaskList>
-    </div>;
-}
--- DinhTuanAnh/Todo-List-App/todo-list/card/card-header/CardHeader.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./style.css";
-
-export function CardHeader(props) {
-    return <div className="card-header">{props.title}</div>;
-}
