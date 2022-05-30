4431282a55ebf44265baadc474b04a9c9886eff3
viethaaa <88701791+viethaaa@users.noreply.github.com>
2022-05-30 08:37:47 +0700

    update

--- src/task-list/TaskList.jsx
+++ src/task-list/TaskList.jsx
@@ -3,13 +3,10 @@ import { Header } from "./hearder/Header";
 import './tasklist.css';
 import { TaskItem } from "./task-item/TaskItem";
 
+
 export function TaskList() {
     return <div className="task_list">
         <Header/>
-        <TaskItem done = {false} label = "Test React Ant Design Todo List"/>
-        <TaskItem done = {false} label = "Write About React And Ant Design"/>
-        <TaskItem done = {true} label = "Finalize Presentation"/>
-        <TaskItem done = {false} label = "Book Flights To Ireland"/>
-
+        <TaskItem/>
     </div>
 }
--- src/task-list/task-item/TaskItem.jsx
+++ src/task-list/task-item/TaskItem.jsx
@@ -2,14 +2,26 @@ import React from "react";
 import { Icon } from "../icon/Icon";
 import './style.css';
 
-export function TaskItem(props) {
-    console.log(props)
-    return <div className="task_item">
-        <div><input type="checkbox" checked = {props.done} name="" id="" /></div>
+const listToDo = [
+    {done: true, describe: "Test React Ant Design Todo List"},
+    {done: true, describe: "Write About React And Ant Design"},
+    {done: false, describe: "Finalize Presentation"},
+    {done: true, describe: "Book Flights To Ireland"}
+]
+
+export function TaskItem() {
+
+    return listToDo.map((item) => {
+        return <div className="task_item">
+    
+    
+            <div><input type="checkbox" checked = {item.done} name="" id="" /></div>
+            
+            <div className={`asdasd ${item.done ? "checked" : ''}`}>
+                {item.describe}
+            </div>
+            <Icon/>
+            </div>
         
-        <div className={`asdasd ${props.done ? "checked" : ''}`}>
-            {props.label}
-        </div>
-        <Icon/>
-    </div>
+    })
 }
