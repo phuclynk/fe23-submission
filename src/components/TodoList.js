d335cfa39b23e632596d9bf084e835c180ef27e8
Tiep <onmyown0105@gmail.com>
2022-05-30 18:08:31 +0700

    react-todo-list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -17,31 +17,6 @@ const TodoList = ({ todos, setTodos, filteredTodos }) => {
                             todo={todo}
                         />
                     ))}
-                    <ul className='todo-list-2'>
-                        <div className='todo'>
-                        <button className="complete-btn"><i className="fas fa-check"></i></button>
-                        <li className="todo-item ">Test React Ant Design Todo List</li>
-                        <button className="trash-btn"><i className="fas fa-trash"></i></button>
-                        </div>
-
-                        <div className='todo'>
-                        <button className="complete-btn"><i className="fas fa-check"></i></button>
-                        <li className="todo-item ">Write About React And Ant Design</li>
-                        <button className="trash-btn"><i className="fas fa-trash"></i></button>
-                        </div>
-
-                        <div className='todo'>
-                        <button className="complete-btn"><i className="fas fa-check"></i></button>
-                        <li className="completed todo-item ">Finalize Presentation</li>
-                        <button className="trash-btn"><i className="fas fa-trash"></i></button>
-                        </div>
-
-                        <div className='todo'>
-                        <button className="complete-btn"><i className="fas fa-check"></i></button>
-                        <li className="todo-item ">Books Flight To Ireland</li>
-                        <button className="trash-btn"><i className="fas fa-trash"></i></button>
-                        </div>
-                    </ul>
                 </ul>
             </div>
         </>
